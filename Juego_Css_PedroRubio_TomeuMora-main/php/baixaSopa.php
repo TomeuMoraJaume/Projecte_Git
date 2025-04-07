f8d361dc476187e7f17ded1569c2a8942f56f5a4
TomeuMoraJaume <tomeumorajaume05@gmail.com>
2025-04-07 09:28:43 +0200

    Css acabat

---
+++ Juego_Css_PedroRubio_TomeuMora-main/php/baixaSopa.php
@@ -0,0 +1,31 @@
+<?php
+// Verificar si el formulario ha sido enviado mediante POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Array de las sopas que pueden ser seleccionadas
+    $sopas = ["s1", "s2", "s3", "s4", "s5"];
+    
+    // Lista para almacenar las sopas seleccionadas
+    $sopasSeleccionadas = [];
+
+    // Recorrer las sopas y verificar si están seleccionadas
+    foreach ($sopas as $sopa) {
+        if (isset($_POST[$sopa])) {
+            // Si el checkbox está marcado, se agrega la sopa a la lista
+            $sopasSeleccionadas[] = $sopa;
+        }
+    }
+    
+    // Mostrar las sopas seleccionadas
+    if (!empty($sopasSeleccionadas)) {
+        echo "Las sopas seleccionadas son:<br>";
+        foreach ($sopasSeleccionadas as $sopa) {
+            // Convertir el nombre del checkbox (s1, s2, etc.) en un nombre legible
+            echo "Sopa " . substr($sopa, -1) . "<br>";
+        }
+    } else {
+        echo "No se ha seleccionado ninguna sopa.";
+    }
+} else {
+    echo "El formulario no se ha enviado.";
+}
+?>
